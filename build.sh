c42cf8f45bf2d833ee5b307bbd2d0ffd008525d8
Socheat Sok <alex@socheat.net>
2021-12-14 11:25:26 +0700

    Build multiple wasm module for different dictionary

--- build.sh
+++ build.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 set -euo pipefail
 
-echo " => Building icu..."
-docker build . --file Dockerfile.icu -t icu-data
-rm -Rf build && mkdir build
-docker run -v "$PWD/build:/opt/mount" --rm "$(docker images -q icu-data)" cp /artifacts/data.h /opt/mount
-cp break_iterator.c icu.py build/
+DICTS=(burmesedict cjdict khmerdict laodict thaidict)
 
-echo " => Building break iterator engine..."
-docker build . --file Dockerfile -t build
-docker run -v "$PWD/src:/opt/mount" --rm "$(docker images -q build)" cp /artifacts/break_iterator.wasm /opt/mount
+for DICT in ${DICTS[@]}; do
+    echo Generate filters.json fot ${DICT}
+    echo '{ "strategy": "additive", "featureFilters": { "brkitr_rules": "include", "brkitr_tree": "include", "cnvalias": "include", "ulayout": "include", "brkitr_dictionaries": { "whitelist": [ "' ${DICT} '" ] } } }' > filters.json
+
+    echo " => Building icu..."
+    docker build . --file Dockerfile.icu -t icu-data
+    rm -Rf build && mkdir build
+    docker run -v "$PWD/build:/opt/mount" --rm "$(docker images -q icu-data)" cp /artifacts/data.h /opt/mount
+    cp break_iterator.c icu.py build/
+
+    echo " => Building break iterator engine..."
+    docker build . --file Dockerfile -t build
+
+    echo " => Copying break_iterator_${DICT}.wasm"
+    docker run -v "$PWD/src:/opt/mount" --rm "$(docker images -q build)" cp /artifacts/break_iterator.wasm /opt/mount/break_iterator_${DICT}.wasm
+done
